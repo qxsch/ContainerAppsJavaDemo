0124d3e11c212dec75a920c12c8b2006d75f2e11
Aymen Furter <aymenfurter@microsoft.com>
2023-04-20 19:30:59 +0200

    fix: fix deployability

--- environments/dev/storage-account-app/main.tf
+++ environments/dev/storage-account-app/main.tf
@@ -3,22 +3,23 @@ module "container_app" {
 
   app_name                            = "storage-account-app"
   environment                         = "dev"
-  container_cpu                       = "0.25"
-  container_memory                    = "0.5Gi"
+  container_cpu                       = "1"
+  container_memory                    = "2Gi"
 
   container_ingress_external_enabled  = "true"
   container_registry_name             = "devacrshared1"
   container_registry_rg               = "rg-dev-aca-shared"
 
-  #linked_storage_accounts = [
-  #  {
-  #    name                = "pocacademodevstorage1"
-  #  }
-  #]
-
-  dapr_storage_accounts = [
+  linked_storage_accounts = [
     {
-      name                = "pocacademodevstorage2"
+      name                = "acstrgaccountlinked001"
     }
   ]
+
+   dapr_storage_accounts = [
+    {
+      name                = "acastrgaccountdapr001"
+    }
+  ]
+
 }
